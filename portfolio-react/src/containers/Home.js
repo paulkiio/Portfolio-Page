90ba76d098b9471a6aa22cf767de7733d68aff0b
paul <pkwdigital@gmail.com>
2017-04-24 09:16:44 +0200

    add BS and Navbar

---
+++ portfolio-react/src/containers/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Portfolio</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
