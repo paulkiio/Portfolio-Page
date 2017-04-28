1bf4101d39693b447bd7d19e0f9a99ec5332fd12
paul <pkwdigital@gmail.com>
2017-04-28 10:21:12 -0400

    add routes and links in nav bar

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import {Navbar} from 'react-bootstrap';
+import {Link, withRouter} from 'react-router-dom';
+import {Navbar, Nav} from 'react-bootstrap';
+import RouteNavItem from './components/RouteNavItem';
 import Routes from './Routes';
 import './App.css';
 
 class App extends Component {
+
+  handleNavLink = (event) => {
+    event.preventDefault();
+    this.props.history(event.currentTarget.getAttribute('href'));
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,6 +22,14 @@ class App extends Component {
             </Navbar.Brand>
             <Navbar.Toggle/>
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <RouteNavItem onclick={this.handleNavLink} href="/about">About</RouteNavItem>
+              <RouteNavItem onclick={this.handleNavLink} href="/Resume">Resume</RouteNavItem>
+              <RouteNavItem onclick={this.handleNavLink} href="/projects">Projects</RouteNavItem>
+              <RouteNavItem onclick={this.handleNavLink} href="/contact">Contact</RouteNavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes/>
       </div>
@@ -22,4 +37,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
