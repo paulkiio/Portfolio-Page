c5227912fb200a4b42e880f69d55cd174c77193e
Paul Wathome <pwathome@Paul-Wathome.local>
2017-05-09 15:38:17 -0400

    add days of the week logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 	$(".button-collapse").sideNav();
 	$('.modal').modal();
-	
+
 	var viewHeight = $(window).height();
 	var meContainer = $('.me-container .avatar').css('height', viewHeight);
 
+	var d = new Date();
+	var weekday = new Array(7);
+	weekday[0] = "Sunday";
+	weekday[1] = "Monday";
+	weekday[2] = "Tuesday";
+	weekday[3] = "Wednesday";
+	weekday[4] = "Thursday";
+	weekday[5] = "Friday";
+	weekday[6] = "Saturday";
+
+	var n = weekday[d.getDay()];
+
+	// document.write("day").innerHTML = n
 });
