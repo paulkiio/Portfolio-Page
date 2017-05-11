9efa7adeaae2e153902cc6a61e1625b10fd088ef
paul <pkwdigital@gmail.com>
2017-05-11 15:10:34 -0400

    add scroll click

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
 $(document).ready(function() {
-	$(".button-collapse").sideNav();
-	$('.modal').modal();
+  $(".button-collapse").sideNav();
+  $('.modal').modal();
 
-	var viewHeight = window.innerHeight ? window.innerHeight : $(window).height();
-	var meContainer = $('.me-container .avatar').css('height', viewHeight);
+  var viewHeight = window.innerHeight
+    ? window.innerHeight
+    : $(window).height();
+  var meContainer = $('.me-container .avatar').css('height', viewHeight);
 
-	var d = new Date();
-	var weekday = new Array(7);
-	weekday[0] = "Sunday";
-	weekday[1] = "Monday";
-	weekday[2] = "Tuesday";
-	weekday[3] = "Wednesday";
-	weekday[4] = "Thursday";
-	weekday[5] = "Friday";
-	weekday[6] = "Saturday";
-	var n = weekday[d.getDay()];
-	document.getElementById("day").innerHTML = n;
+  var d = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  var n = weekday[d.getDay()];
+  document.getElementById("day").innerHTML = n;
+
+  var $root = $('html, body');
+  $('a').click(function() {
+    $root.animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
+  });
 
 });
